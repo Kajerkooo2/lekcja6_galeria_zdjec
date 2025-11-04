6ee81125e3c1c0cac1e409e940a9c1910791bfad
Kajerkooo2 <prokajerko79@gmail.com>
2025-11-04 08:24:48 +0100

    Budowa glownego widoku 'Main Activity'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.program_galeria"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.program_galeria"
